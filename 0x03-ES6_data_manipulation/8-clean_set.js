134355c68d3ccca58a40640b2b43a70997768d08
opegol <opgbenga@gmail.com>
2024-07-03 00:09:19 +0000

    Edits to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,14 @@ export default function cleanSet(set, startString) {
   const restArr = [];
   if (
     startString.length === 0
-    || typeof set !== 'object'
-    || typeof startString !== 'string'
+    || set.size === 0
+    //|| typeof set !== 'object'
+    //|| typeof startString !== 'string'
   ) {
     return '';
   }
   for (const elem of set) {
-    if (elem && elem.startsWith(startString)) {
+    if (elem.startsWith(startString)) {
       restArr.push(elem.slice(startString.length));
     }
   }
